b3d668f6c8b99362c33647d8ebfbaa08c45e6894
Sohini Kar <sohinikar21@gmail.com>
2022-05-04 00:18:26 -0400

    Fixed sandbox

--- recognizer/sandbox.py
+++ recognizer/sandbox.py
@@ -7,22 +7,20 @@ async def start_turn():
     print("do start turn thigns done")
 
 
-def post_post_start_turn():
+async def post_post_start_turn():
+    asyncio.create_task(start_turn())  # fire and forget async_foo()
 
     print('Do some actions 1')
+    await asyncio.sleep(1)
     print('Do some actions 2')
+    await asyncio.sleep(1)
     print('Do some actions 3')
 
 
-async def post_start_turn():
-    asyncio.create_task(start_turn())  # fire and forget async_foo()
+def post_start_turn():
     
     loop = asyncio.get_event_loop()
     loop.run_until_complete(post_post_start_turn())
     
-def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(post_start_turn())
     
-    
-main()
+post_start_turn()
