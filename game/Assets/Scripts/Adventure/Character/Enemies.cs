1a58b88187e72378ebd47288194b32f74160b5bf
Sohini Kar <sohinikar21@gmail.com>
2022-05-04 23:35:45 -0400

    Added more enemies

--- game/Assets/Scripts/Adventure/Character/Enemies.cs
+++ game/Assets/Scripts/Adventure/Character/Enemies.cs
@@ -10,3 +10,30 @@ public class Werewolf : Character
     // public override float PlayerTurnTime => playerTurnTime;
 }
 
+public class Orc : Character
+{
+    public override string Name => "Orc";
+    public override float MaxHealth => 70f;
+    public override float AttackDamage => 13f;
+    // public override float DefenseMultiplier => 1f;
+    // public override float PlayerTurnTime => playerTurnTime;
+}
+
+public class Goblin : Character
+{
+    public override string Name => "Goblin";
+    public override float MaxHealth => 120f;
+    public override float AttackDamage => 10f;
+    // public override float DefenseMultiplier => 1f;
+    // public override float PlayerTurnTime => playerTurnTime;
+}
+
+public class Minotaur : Character
+{
+    public override string Name => "Minotaur";
+    public override float MaxHealth => 140f;
+    public override float AttackDamage => 14f;
+    // public override float DefenseMultiplier => 1f;
+    // public override float PlayerTurnTime => playerTurnTime;
+}
+
